7aa18b76d352f893cd85f829d5b2740971b73ad0
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-08-13 19:16:20 -0300

    API Fluent used

--- eShop.ProductApi/Context/AppDbContext.cs
+++ eShop.ProductApi/Context/AppDbContext.cs
@@ -9,4 +9,18 @@ public class AppDbContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        //category
+        mb.Entity<Category>().HasKey(c => c.CategoryId);
+
+        mb.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        //product
+        
+    }
 }
