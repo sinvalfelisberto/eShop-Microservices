bff1e972aa3f9281970bd2fad2e12955b1469b82
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-08-16 21:36:15 -0300

    mapping simplified

--- eShop.ProductApi/DTOs/Mappings/MappingProfile.cs
+++ eShop.ProductApi/DTOs/Mappings/MappingProfile.cs
@@ -9,12 +9,10 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
         CreateMap<Product, ProductDTO>()
             .ForMember(x => x.CategoryName, opt => opt
                         .MapFrom(src => src.Category.Name));
-        CreateMap<ProductDTO, Product>();
-        CreateMap<CategoryDTO, Category>();
-        
     }   
 
 }
